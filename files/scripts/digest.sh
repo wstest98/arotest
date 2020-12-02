eb9a158e9da17d918f4170a9dec2422d3a62d65e
root <wonsik98@naver.com>
2020-12-02 09:31:46 +0900

    ok

--- files/scripts/digest.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-BASEDIR=$(dirname "$0")
-. $BASEDIR/env.sh
-
-unset JAVA_OPTS
-
-${CATALINA_HOME}/bin/digest.sh -a sha-256 "$@" 
--- files/scripts/urlCheck.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-curl -I 10.65.40.215:8180/simple/failover.jsp  > /dev/null  2>&1 
